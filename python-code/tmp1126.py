7ce8b82e1b1f8f0d18025d414f69d2491df6a41c
liudongjie <ldjwyyx@163.com>
2016-12-18 22:20:52 +0800

    handle excel

--- python-code/tmp1126.py
+++ python-code/tmp1126.py
@@ -1,10 +1,16 @@
 #coding=utf-8
-fp = open(r'f:\1.txt')
-aList = []
-for item in fp:
-    if item.strip():
-        aList.append(item)
-fp.close()
-fp = open(r'f:\4.txt','w')
-fp.writelines(aList)
-fp.close()
+from xlrd import open_workbook
+from xlwt import Workbook
+
+test_file=open_workbook('f:\\test.xlsx')
+test_sheet=test_file.sheets()[0]
+result=Workbook()
+result_sheet=result.add_sheet('result',cell_overwrite_ok=True)
+count=0
+for row in range(1,test_sheet.nrows):
+	for col in range(1,test_sheet.ncols):
+		if test_sheet.cell(row,1).value<=test_sheet.cell(row,2).value:
+			result_sheet.write(row,1,test_sheet.cell(row,1).value)
+			count+=1
+result_sheet.write(0,0,count/2)
+result.save('f:\\result.xls')
