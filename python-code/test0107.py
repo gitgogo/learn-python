4ca137b0257c79525a089e89d9b0850de470e796
liudongjie <ldjwyyx@163.com>
2017-01-10 23:35:40 +0800

    周二课

--- python-code/test0107.py
+++ python-code/test0107.py
@@ -50,3 +50,57 @@ print newTime.strftime('%Y-%m-%d %H:%M:%S')
 print datetime.datetime.now()+datetime.timedelta(days=100)
 print datetime.datetime.now()-datetime.timedelta(hours=30)
 print datetime.timedelta(hours=1,seconds=20).total_seconds()
+
+#coding=utf-8
+import time,datetime,calendar,os
+# print [x for x in dir(calendar.Calendar) if not x.startswith('_')]
+# print [x for x in dir(calendar) if not x.startswith('_')]
+# my_cal=calendar.HTMLCalendar(calendar.MONDAY)
+# print my_cal.formatmonth(2017,1)
+# with open('/Users/ralphliu/Document/mycal.html','w') as f:
+# 	f.write(my_cal.formatmonth(2017,1))
+
+# start_time=time.time()
+# fp=open('f:\\tmp.rmvb','wb')
+# file=u'f:\\电影\\猎头者BD中字1280高清.rmvb'
+# with open(file,'rb') as f:
+# 	for line in f:
+# 		tmp=line
+# # fp.close()
+# total_time=time.time()-start_time
+# print u'文件大小：%s  用时：%s'%(os.path.getsize(file),total_time)
+# def get_date():
+# 	return time.strftime('%Y||%m||%d',time.localtime())
+
+# def get_time():
+# 	return time.strftime('%H%%%M%%%S',time.localtime())
+
+# def get_date_and_time():
+# 	return time.strftime('%Y||%m||%d   %H%%%M%%%S',time.localtime())
+
+# print get_date()
+# print get_time()
+# print get_date_and_time()
+
+# format_time=time.strptime('2017||01||10   21%45%15','%Y||%m||%d   %H%%%M%%%S')
+# print time.mktime(format_time)
+
+import os,time
+def make_dir(path):
+	os.chdir(path)
+	# name_one=str((datetime.datetime.now()-datetime.timedelta(days=300)).year)
+	name_one=str(time.localtime().tm_year-1)
+	os.mkdir(name_one)
+	os.chdir(name_one)
+	name_two=str(time.localtime().tm_mon-1)
+	if name_two=='0':
+		name_two='12'
+	os.mkdir(name_two)
+	os.chdir(name_two)
+	name_three=str(datetime.date.today())
+	os.mkdir(name_three)
+	file_name=time.strftime('%H%M%S')
+	with open(os.path.join(name_three,file_name)+'.txt','w') as f:
+		f.write(u'今天是今年的第%s天'%(time.localtime().tm_yday)+
+			'\n'+u'今年的第%s周'%(time.strftime('%U')))
+make_dir('f:\\')
--- python-code/tmp.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-import time,datetime,calendar
-# print [x for x in dir(calendar.Calendar) if not x.startswith('_')]
-# print [x for x in dir(calendar) if not x.startswith('_')]
-my_cal=calendar.HTMLCalendar(calendar.MONDAY)
-print my_cal.formatmonth(2017,1)
-with open('/Users/ralphliu/Document/mycal.html','w') as f:
-	f.write(my_cal.formatmonth(2017,1))
