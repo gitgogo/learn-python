63cef187a34eab5a1d35821bbe1c0caacd1eaee7
liudongjie <ldjwyyx@163.com>
2017-02-18 08:25:46 +0800

    exercise likeDict

--- python-code/tmp.py
+++ python-code/tmp.py
@@ -1,15 +1,20 @@
 #coding=utf-8
-class a(object):
-	def __init__(self):
-		print 'init...'
+class LikeDict(object):
+	def __init__(self,attribut):
+		self.attribut=attribut
 
-	def __new__(cls,*arg):
-		print 'new...'
-		return object.__new__(cls,*args)
+	def __getitem__(self,key):
+		print self.attribut[key]
 
-	def s(self):
-		print 123
+	def __setitem__(self,key,value):
+		self.attribut[key]=value
 
-# print a.__dict__
-b=a()
-b.s()
+	def __delitem__(self,key):
+		del attribut[key]
+
+Dict={}
+likeDict=LikeDict(Dict)
+likeDict['a']=1
+likeDict['b']=2
+likeDict['b']
+likeDict['a']
